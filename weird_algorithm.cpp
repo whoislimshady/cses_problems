2c5c6adf8e7b0ea26574e194fc8f7739c18b877d
Harsh <72891217+whoislimshady@users.noreply.github.com>
2021-06-10 00:22:37 +0530

    Update weird_algorithm.cpp

--- weird_algorithm.cpp
+++ weird_algorithm.cpp
@@ -1,22 +1,32 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
+ 
+typedef long long ll;
+ 
 int main()
 {
-    int n;
-    cin >> n;
-    cout<<n<<" ";
-    while (n != 1)
-    {   
-        if (n % 2 == 0)
-        {
-           n= n / 2;
-        }
-        else
-        {
-            n = n * 3 + 1;
-        }
-        cout << n<<" ";
-    }
-
-    return 0;
+ 
+	ll n;
+	cin >> n;
+	cout << n << " ";
+	while (n != 1)
+	{
+		if (n % 2 == 0)
+		{
+			n = n / 2;
+ 
+		}
+		else
+		{
+			n = (n * 3) + 1;
+		}
+		cout << n << " ";
+ 
+	}
+	return 0;
+ 
+ 
+ 
+ 
+ 
 }
